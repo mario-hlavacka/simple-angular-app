1a382ee00ae8af6392362474e23e7dfa882967ff
Mario Hlavacka <mario.hlavacka@gmail.com>
2025-04-18 21:58:00 +0200

    feat: create basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { DonateFormComponent } from './donate-form/donate-form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, DonateFormComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/donate-form/donate-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donate-form',
+  imports: [],
+  templateUrl: './donate-form.component.html',
+  styleUrl: './donate-form.component.scss'
+})
+export class DonateFormComponent {
+
+}
