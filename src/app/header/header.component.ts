e7194bcfbad383356e2a67213518c7336e78e5de
Mario Hlavacka <mario.hlavacka@gmail.com>
2025-04-18 22:04:42 +0200

    feat: add fontawesome icons + some content

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  faFacebookF = faFacebookF;
+  faInstagram = faInstagram;
 }
